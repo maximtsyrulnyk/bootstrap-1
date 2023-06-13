42fbff42a74d587313adfee4866dfcef767e1581
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2023-06-13 13:26:24 +0300

    create project Module3

--- src/route/index.js
+++ src/route/index.js
@@ -12,11 +12,51 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+    layout: 'default',
+    title: 'Home page',
+    text: 'Click me',
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
+router.get('/users', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('user-list', {
+    layout: 'header',
+    title: 'User list page',
+
+    data: {
+      userlist: [
+        {
+          id: 5123,
+          name: 'Ivan',
+          isAdmin: true,
+          isConfirm: true,
+          balance: 100,
+        },
+        {
+          id: 6843,
+          name: 'Max',
+          isAdmin: false,
+          isConfirm: false,
+          balance: 0,
+        },
+        {
+          id: 6843,
+          name: 'Vasya',
+          isAdmin: true,
+          isConfirm: true,
+          balance: null,
+        },
+      ],
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
